913281b4696ae34f595dc84b92e360bd9b218135
forsigner <forsigner@gmail.com>
2019-10-11 16:33:29 +0800

    feat: update template

--- templates/with-router/src/pages/about.tsx
+++ templates/with-router/src/pages/about.tsx
@@ -1,3 +1,13 @@
 import React from 'react'
+import { Link } from '@peajs/router'
 
-export default () => <div>About me</div>
+export default () => (
+  <div>
+    <h1>About Page</h1>
+    <nav>
+      <Link to="/">Home</Link>
+      <div>|</div>
+      <Link to="/about">About</Link>
+    </nav>
+  </div>
+)
--- templates/with-router/src/pages/index.tsx
+++ templates/with-router/src/pages/index.tsx
@@ -1,3 +1,13 @@
 import React from 'react'
+import { Link } from '@peajs/router'
 
-export default () => <div>Hi pea</div>
+export default () => (
+  <div>
+    <h1>Home Page</h1>
+    <nav>
+      <Link to="/">Home</Link>
+      <div>|</div>
+      <Link to="/about">About</Link>
+    </nav>
+  </div>
+)
